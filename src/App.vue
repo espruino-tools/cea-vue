f0442b903fc2ecead2d5e3a258acaba50172f579
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-25 13:27:56 +0100

    populated cea-vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-on:click="connect()">connect</button>
+</template>
+
+<script>
+import {DeviceController} from '@espruino-tools/device-controller'
+
+const device = new DeviceController()
+
+export default {
+  name: 'App',
+  methods: {
+    connect(){
+      device.connect(function(){
+        alert('connected');
+      })
+    }
+  }
+}
+</script>
